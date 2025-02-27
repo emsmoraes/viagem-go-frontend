af04c81376e685a980d6ac5fd1b7bbd0866e60b3
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-02-27 19:10:24 -0300

    feat: Adicionar componente de botão e configurações do shadcn-ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
       <h1 className="text-3xl font-bold bg-mint-500">Bem-vindo ao Aurora!</h1>
+      <Button className="mt-2">Clique aqui</Button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import * as path from "path";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
