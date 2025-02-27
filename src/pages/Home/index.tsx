8323446197ebb6969e9fb2f77611654b9cf9409c
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-02-27 18:39:25 -0300

    feat: Adicionar Tailwind CSS e configurações globais

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold">Bem-vindo ao Aurora!</h1>
+      <h1 className="text-3xl font-bold bg-mint-500">Bem-vindo ao Aurora!</h1>
     </div>
   );
 }
