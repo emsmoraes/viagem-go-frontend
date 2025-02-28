37cd6764e2288247885d29abf5cb6609e9a2af0c
Nicholas Nascimento <nicholas.an99@gmail.com>
2025-02-28 09:08:49 -0300

    feat: Configuracoes iniciais do projeot

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
+import * as path from "path";
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import * as path from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
