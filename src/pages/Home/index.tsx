e24e6334664479f5cabf7a4fdb8f328277a3824f
Nicholas Nascimento <nicholas.an99@gmail.com>
2025-02-28 15:51:20 -0300

    feat: Finalizando componentes da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
 import { Header } from "@/shared/components/layout/Header";
 import { Sidebar } from "@/shared/components/layout/Sidebar";
+import { MetricsCard } from "@/shared/components/ui/MetricsCard";
 
 export function Home() {
+  const metrics = [
+    { title: "Métrica 1", value: 240, description: "Aumento de 14% vs semana anterior" },
+    { title: "Métrica 2", value: 240, description: "Aumento de 14% vs semana anterior" },
+    { title: "Métrica 3", value: 240, description: "Aumento de 14% vs semana anterior" },
+    { title: "Métrica 4", value: 240, description: "Aumento de 14% vs semana anterior" },
+  ];
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -11,6 +19,20 @@ export function Home() {
 
         <div className="flex flex-col flex-1 gap-4 p-4 bg-[#F7F7F7] overflow-y-auto h-full">
           <h1 className="my-2 text-[24px] font-bold text-[#1D1D1D]">Dashboard</h1>
+
+          <div className="flex flex-col gap-4">
+            {metrics.map((metric, index) => (
+              <MetricsCard key={index} {...metric} />
+            ))}
+          </div>
+
+          <div className="w-full bg-white rounded-lg shadow-md p-4 flex flex-col gap-2">
+            <h2 className="text-[16px] font-semibold text-[#1d1d1d]">Atividade Recente</h2>
+
+            <div className="w-full h-[420px] bg-[#D0D0D0] rounded-lg flex items-center justify-center">
+              <p className="text-[#585858] text-[14px]">Gráfico de atividade</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
