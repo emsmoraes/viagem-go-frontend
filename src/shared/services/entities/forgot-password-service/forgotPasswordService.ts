30c216d7659fe1960d358c6eadb111e9eb418202
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-03-02 10:46:16 -0300

    chore: correcão do retorno de erros

--- src/shared/services/entities/forgot-password-service/forgotPasswordService.ts
+++ src/shared/services/entities/forgot-password-service/forgotPasswordService.ts
@@ -4,16 +4,20 @@ import { ApiException } from "@/shared/services/api-exception/ApiException";
 const forgotPassword = async (email: string, redirectUrl: string): Promise<void> => {
   try {
     await api.post("forgot-password", { email, redirectUrl });
-  } catch (error) {
-    throw new ApiException(error instanceof Error ? error.message : "Erro desconhecido");
+  } catch (error: any) {
+    throw new ApiException(
+      error?.response?.data?.message ?? (error instanceof Error ? error.message : "Erro desconhecido"),
+    );
   }
 };
 
 const updatePassword = async (key: string, password: string, passwordConfirm: string): Promise<void> => {
   try {
     await api.patch(`forgot-password/${key}`, { password, passwordConfirm });
-  } catch (error) {
-    throw new ApiException(error instanceof Error ? error.message : "Erro desconhecido");
+  } catch (error: any) {
+    throw new ApiException(
+      error?.response?.data?.message ?? (error instanceof Error ? error.message : "Erro desconhecido"),
+    );
   }
 };
 
