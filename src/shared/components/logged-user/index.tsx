9292ef72fe4451a105466f55cf16d76904ed161a
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-03-13 21:18:22 -0300

    feat: add user logout

--- src/shared/components/logged-user/index.tsx
+++ src/shared/components/logged-user/index.tsx
@@ -1,25 +1,43 @@
 import { authStore } from "@/shared/store/auth.store";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Popover, PopoverTrigger } from "../ui/popover";
+import { LogOutIcon } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 const LoggedUser = () => {
-  const { user } = authStore();
+  const { user, logout } = authStore();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    queryClient.clear();
+    logout();
+    navigate("/sign-in");
+  };
 
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <div className="flex cursor-pointer items-center gap-2 duration-200 hover:bg-gray-100 hover:opacity-90 p-3 rounded-xl">
-          <Avatar>
-            <AvatarImage
-              src="https://upload.wikimedia.org/wikipedia/it/thumb/8/81/Fredfl.jpg/260px-Fredfl.jpg"
-              alt="avatar"
-            />
-            <AvatarFallback>{user?.name?.slice(0, 1).toUpperCase()}</AvatarFallback>
-          </Avatar>
+        <div className="flex cursor-pointer items-center gap-2 rounded-xl pr-0 duration-200 hover:bg-gray-100 hover:opacity-90">
+          <div className="flex flex-grow items-center gap-2 py-3 pl-3 hover:text-blue-500">
+            <Avatar>
+              <AvatarImage
+                src="https://upload.wikimedia.org/wikipedia/it/thumb/8/81/Fredfl.jpg/260px-Fredfl.jpg"
+                alt="avatar"
+              />
+              <AvatarFallback>{user?.name?.slice(0, 1).toUpperCase()}</AvatarFallback>
+            </Avatar>
+            <p className="truncate overflow-hidden text-[16px] font-[400] text-nowrap text-ellipsis text-black">
+              {user?.name.split(" ")[0]}
+            </p>
+          </div>
 
-          <p className="overflow-hidden truncate text-ellipsis text-nowrap text-[16px] font-[400] text-black">
-            {user?.name.split(" ")[0]}
-          </p>
+          <div className="group flex h-full items-center gap-2 rounded-r-lg p-1 px-3 hover:bg-red-100/80">
+            <button className="h-full text-gray-500 group-hover:text-red-500" onClick={handleLogout}>
+              <LogOutIcon />
+            </button>
+          </div>
         </div>
       </PopoverTrigger>
     </Popover>
